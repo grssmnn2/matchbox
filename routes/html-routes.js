f711014ca1f92830c0b4ca343913f1f697835325
Rachel Brown <rbrown511@gmail.com>
2018-03-18 14:57:25 -0500

    organizing public file structure

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,34 +1,31 @@
-// Dependencies
-// =============================================================
-var path = require("path");
+(() => {
 
-// Routes
-// =============================================================
-module.exports = function(app) {
+  const path = require("path");
+  const db = require("../models");
 
-  // root route sends user to main welcome page
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+  module.exports = app => {
 
-  //send user to login page (may end up a pop up modal)
-  app.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
+    // homepage - login or signup
+    app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname, "./public/index.html"));
+    });
 
-  //send user to survey page
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-  });
+    // login page
+    app.get("/login", (req, res) => {
+      res.sendFile(path.join(__dirname, "./public/login.html"));
+    });
 
-   //send user to box results page (may end up being a modal)
-   app.get("/results", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/results.html"));
-  });
+    // survey page
+    app.get("/survey", (req, res) => {
+      res.sendFile(path.join(__dirname, "./public/survey.html"));
+    });
 
-  //send user to checkout/end page
-  app.get("/checkout", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/checkout.html"));
-  });
+    // user dashboard (handlebars)
+    app.get("/:id", (req, res) => {
+      db.UserBox.findOne({ where: { id: req.params.id }
+      }).then(data => res.render("index", { user: data }));
+    });
 
-};
+  };
+
+})();
