8e43b1ae9b1c72448032a7ce3ddcb887fcd00df2
Rachel Brown <rbrown511@gmail.com>
2018-03-18 18:07:58 -0500

    survey logic completed

---
+++ public/assets/js/survey.js
@@ -0,0 +1,32 @@
+(() => {
+
+    // calculates the answer with highest frequency
+    function mode(arr){
+        return arr.sort((a,b) =>
+              arr.filter(v => v===a).length
+            - arr.filter(v => v===b).length
+        ).pop();
+    }
+
+    // attaching bucket_id to a new user
+    $("#submit-survey").on("click", e => {
+        e.preventDefault();
+        // ===== for testing until login works =====
+        localStorage.setItem("matchBox_user_id", 4);
+        // ===== remove between these lines =====
+        let userId = JSON.parse(localStorage.getItem("matchBox_user_id"));
+        let scoreArray = [];
+        for (let i = 1; i < 7; i++) {
+            let score = $(`input[name=group${i}]:checked`).val();
+            if (score === undefined) { score = Math.floor(Math.random() * 5) + 1 }
+            scoreArray.push(+score);
+        }        
+        let ans = mode(scoreArray);
+        $.ajax("/api/users/" + userId, {
+            type: "PUT",
+            data: { bucket_id: ans }
+        }).then(data => location.reload());
+        console.log(`UPDATE bucket_id TO ${ans} WHERE id=${userId}`);
+    })
+
+})();
--- routes/api-user.js
+++ routes/api-user.js
@@ -29,16 +29,9 @@ module.exports = function (app) {
     });
 
     // PUT route for updating users
-    app.put("/api/users", function (req, res) {
-        db.User.update(req.body,
-            {
-                where: {
-                    id: req.body.id
-                }
-            })
-            .then(function (dbUser) {
-                res.json(dbUser);
-            });
+    app.put("/api/users/:id", (req, res) => {
+        db.User.update(req.body, { where: { id: req.params.id }
+        }).then(dbUser => res.json(dbUser));
     });
 };
 
