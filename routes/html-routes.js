28cec3aa31a5f2a71ee88a7af50d16ce714783f7
Rachel Brown <rbrown511@gmail.com>
2018-03-18 15:08:57 -0500

    inadvertantly deleted second '.'

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,17 +7,17 @@
 
     // homepage - login or signup
     app.get("/", (req, res) => {
-      res.sendFile(path.join(__dirname, "./public/index.html"));
+      res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
     // login page
     app.get("/login", (req, res) => {
-      res.sendFile(path.join(__dirname, "./public/login.html"));
+      res.sendFile(path.join(__dirname, "../public/login.html"));
     });
 
     // survey page
     app.get("/survey", (req, res) => {
-      res.sendFile(path.join(__dirname, "./public/survey.html"));
+      res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
     // user dashboard (handlebars)
