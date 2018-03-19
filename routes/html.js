2fa3c4bb826cce1d0f90f905b65f58c0751be086
Rachel Brown <rbrown511@gmail.com>
2018-03-18 20:30:06 -0500

    updating html route for models

--- routes/html.js
+++ routes/html.js
@@ -23,9 +23,9 @@
     // user dashboard (handlebars)
     app.get("/user_dashboard/:id/:bucket?", (req, res) => {
       Promise.all([
-        db.Boxes.findAll({ where: { bucket_id: req.params.bucket } }), 
-        db.Users.findOne({ where: { id: req.params.id }}), 
-        db.UserBoxes.findAll({ where: { id: req.params.id }})
+        db.Box.findAll({ where: { bucket_id: req.params.bucket } }), 
+        db.User.findOne({ where: { id: req.params.id }}), 
+        db.UserBox.findAll({ where: { id: req.params.id }})
       ]).then(([boxResults, userProfile, pastOrders]) => {
         res.render("index", { boxResults, userProfile, pastOrders });
       })
