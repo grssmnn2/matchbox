7e55d9d68b8ff96a32e6349db241f8faf3c9b7ee
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-03-20 20:09:17 -0500

    Adding POST REQ FIELDS from cecily and Priya

--- public/assets/js/userSignup.js
+++ public/assets/js/userSignup.js
@@ -10,10 +10,21 @@ $(document).ready(function() {
     signUpForm.on("submit", function(event) {
       event.preventDefault();
       var user = {
-        email: emailInput.val().trim(),
-        password: passwordInput.val().trim(),
-        firstName: firstNameInput.val().trim(),
-        lastName: lastNameInput.val().trim()
+        firstName: $("#inputFirstName").val().trim(),
+        lastName: $("#inputLastName").val().trim(),
+        email: $("#inputEmail").val().trim(),
+        password: $("#inputPass").val().trim(),
+        bucket_id: localStorage.getItem("matchBox_bucket_id"),
+        ship_add1: $("#inputAddress").val().trim(),
+        ship_add2: $("#inputAddress2").val().trim(),
+        ship_city: $("#inputCity").val().trim(),
+        ship_state: $("#inputState").val().trim(),
+        ship_zip: $("#inputZip").val().trim(),
+        bill_add1: $("#bAddress").val().trim(),
+        bill_add2: $("#bAddress2").val().trim(),
+        bill_city: $("#bCity").val().trim(),
+        bill_state: $("#bState").val().trim(),
+        bill_zip: $("#bZip").val().trim()
       };
 
       console.log(user);
