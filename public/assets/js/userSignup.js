37cbadfb66ba9c092888063128184a9acf98f32a
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-03-20 21:04:47 -0500

    trying to fix .then

--- public/assets/js/userSignup.js
+++ public/assets/js/userSignup.js
@@ -1,58 +1,93 @@
-console.log("we are linked");
+console.log("we are linked")
 $(document).ready(function() {
-    // Getting references to our form and input
-    var signUpForm = $("form");
-    var emailInput = $("input#inputEmail");
-    var passwordInput = $("input#inputPass");
-    var firstNameInput = $("#inputFirstName");
-    var lastNameInput = $("#inputLastName");
-    // When the signup button is clicked, we validate the email and password are not blank
-    signUpForm.on("submit", function(event) {
-      event.preventDefault();
-      var user = {
-        firstName: $("#inputFirstName").val().trim(),
-        lastName: $("#inputLastName").val().trim(),
-        email: $("#inputEmail").val().trim(),
-        password: $("#inputPass").val().trim(),
-        bucket_id: localStorage.getItem("matchBox_bucket_id"),
-        ship_add1: $("#inputAddress").val().trim(),
-        ship_add2: $("#inputAddress2").val().trim(),
-        ship_city: $("#inputCity").val().trim(),
-        ship_state: $("#inputState").val().trim(),
-        ship_zip: $("#inputZip").val().trim(),
-        bill_add1: $("#bAddress").val().trim(),
-        bill_add2: $("#bAddress2").val().trim(),
-        bill_city: $("#bCity").val().trim(),
-        bill_state: $("#bState").val().trim(),
-        bill_zip: $("#bZip").val().trim()
-      };
+  // Getting references to our form and input
 
-      console.log(user);
+  // var emailInput = $("input#inputEmail");
+  // var passwordInput = $("input#inputPass");
+  // var firstNameInput = $("#inputFirstName");
+  // var lastNameInput = $("#inputLastName");
+  // When the signup button is clicked, we validate the email and password are not blank
+
+  $("form").on("submit", function(event) {
+    event.preventDefault()
+    var user = {
+      firstName: $("#inputFirstName")
+        .val()
+        .trim(),
+      lastName: $("#inputLastName")
+        .val()
+        .trim(),
+      email: $("input#inputEmail")
+        .val()
+        .trim(),
+      password: $("input#inputPass")
+        .val()
+        .trim(),
+      bucket_id: localStorage.getItem("matchBox_bucket_id"),
+      ship_add1: $("#inputAddress")
+        .val()
+        .trim(),
+      ship_add2: $("#inputAddress2")
+        .val()
+        .trim(),
+      ship_city: $("#inputCity")
+        .val()
+        .trim(),
+      ship_state: $("#inputState")
+        .val()
+        .trim(),
+      ship_zip: $("#inputZip")
+        .val()
+        .trim(),
+      bill_add1: $("#bAddress")
+        .val()
+        .trim(),
+      bill_add2: $("#bAddress2")
+        .val()
+        .trim(),
+      bill_city: $("#bCity")
+        .val()
+        .trim(),
+      bill_state: $("#bState")
+        .val()
+        .trim(),
+      bill_zip: $("#bZip")
+        .val()
+        .trim()
+    }
+
+    console.log(user)
   
-      if (!user.email || !user.password) {
-        return;
-      }
-      // If we have an email and password, run the signUpUser function
-      signUpUser(user.email, user.password);
-      emailInput.val("");
-      passwordInput.val("");
-    });
-  
-    // Does a post to the signup route. If succesful, we are redirected to the members page
-    // Otherwise we log any errors
-    function signUpUser(email, password) {
-      $.post("/api/users", {
-        email: email,
-        password: password
-      }).then(function(data) {
-        window.location.replace(data);
+ 
+
+    if (!user.email || !user.password) {
+      return
+    }
+    // If we have an email and password, run the signUpUser function
+    signUpUser(user)
+    // emailInput.val("");
+    // passwordInput.val("");
+  })
+
+  // Does a post to the signup route. If succesful, we are redirected to the members page
+  // Otherwise we log any errors
+  function signUpUser(data, err) {
+    $.post("/api/users", data)
+      .then(function(data) {
+        let id = data.body.id;
+        let bucket_id = data.body.bucket_id;
+        // window.location.replace(data);
         // If there's an error, handle it by throwing up a boostrap alert
-      }).catch(handleLoginErr);
-    }
-  
-    function handleLoginErr(err) {
-      $("#alert .msg").text(err.responseJSON);
-      $("#alert").fadeIn(500);
-    }
-  });
-  
+        // window.location.replace("./error.html");
+        window.location.replace(`./user_dashboard/${id}/${bucket_id}`);
+        console.log("redirect");
+      })
+      .catch(handleLoginErr)
+  }
+
+  function handleLoginErr(err) {
+    // $("#alert .msg").text(err.responseJSON);
+    // $("#alert").fadeIn(500);
+    window.location.replace("./error.html")
+  }
+})
